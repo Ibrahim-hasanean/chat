260ee1b868567e20a906e80046e0a2630c256f01
ibrahim <imh19970408@gmail.com>
2020-06-25 14:08:39 +0300

    add cors

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const bodyParser = require("body-parser");
 const io = require("socket.io")(http);
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
 app.get("/", (req, res) => {
-  res.send("socket suppose to be connected");
+  res.send("socket suppose to be connected on /chat ");
 });
 
 let ns = io.of("/chat");
