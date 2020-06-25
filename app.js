992242669de7baed4711ee6b7339eb36ec9769e9
ibrahim <imh19970408@gmail.com>
2020-06-25 15:02:00 +0300

    add npm start

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ ns.on("connection", (socket) => {
   console.log("user connected");
   socket.on("send_message", (data) => {
     console.log(data);
-    io.of("chat").emit("recive_message", data);
+    io.of("chat").emit("recive_message", "ibrahim");
   });
 });
 
