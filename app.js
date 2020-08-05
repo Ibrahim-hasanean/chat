157159524064296b42b051bf40c150e9fe9402f4
ibrahim <imh19970408@gmail.com>
2020-08-05 14:33:52 +0300

    add id

--- app.js
+++ app.js
@@ -116,7 +116,13 @@ io.on("connection", (socket) => {
       console.log(data);
       //io.sockets.emit("recive_message", data);
       console.log(socket.room);
-      socket.in(socket.room).emit("recive_message", data);
+      socket.in(socket.room).emit("recive_message", {
+        senderName,
+        msg,
+        hour,
+        minutes,
+        reciverName,
+      });
       // socket.emit("recive_message", msg);
 
       console.log(msg);
